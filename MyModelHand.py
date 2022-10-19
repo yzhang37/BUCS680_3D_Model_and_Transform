d3e2b53e6db944323dc9d42e11198b56452d4638
青椒 <johnyin@bu.edu>
2022-10-18 21:21:23 -0400

    code style fixs

--- MyModelHand.py
+++ MyModelHand.py
@@ -44,169 +44,176 @@ class MyModelHand(MultiColorComponent):
         finger_radius = palm_thickness / 2
         finger_length = 1.5 * palm_thickness
 
+        wrist_part.setRotateExtent(wrist_part.uAxis, -45, 15)
+        palm_part.setRotateExtent(wrist_part.uAxis, -45, 15)
+        palm_part.setRotateExtent(wrist_part.wAxis, 0, 0)
         if not left_handed:
-            wrist_part.setRotateExtent(wrist_part.uAxis, -45, 15)
             wrist_part.setRotateExtent(wrist_part.vAxis, -5, 15)
             wrist_part.setRotateExtent(wrist_part.wAxis, -90, 45)
 
-            palm_part.setRotateExtent(wrist_part.uAxis, -45, 15)
             palm_part.setRotateExtent(wrist_part.vAxis, -5, 15)
-            palm_part.setRotateExtent(wrist_part.wAxis, 0, 0)
 
-            thumb_finger = MyModelFinger(self, Point((-palm_length / 2, 0, palm_length / 4)), shaderProg,
-                                         finger_radius=finger_radius, finger_height=finger_length, finger_nums=2)
-            thumb_finger.setDefaultAngle(-45, thumb_finger.vAxis)
-            palm_part.addChild(thumb_finger)
-            self.componentList += thumb_finger.components
-            for k, v in thumb_finger.componentDict.items():
+            thumb_finger_part = MyModelFinger(self, Point((
+                -palm_length / 2, 0, palm_length / 4)), shaderProg, finger_radius=finger_radius,
+                                              finger_height=finger_length, finger_nums=2)
+            thumb_finger_part.setDefaultAngle(-45, thumb_finger_part.vAxis)
+            palm_part.addChild(thumb_finger_part)
+            self.componentList.extend(thumb_finger_part.components)
+            for k, v in thumb_finger_part.componentDict.items():
                 self.componentDict[f'thumb_{k}'] = v
 
-            Index_finger = MyModelFinger(self, Point((-palm_length / 2 + finger_radius, 0, palm_length)), shaderProg,
+            index_finger_part = MyModelFinger(self, Point((
+                -palm_length / 2 + finger_radius, 0, palm_length)), shaderProg,
                                          finger_radius=finger_radius, finger_height=0.9 * finger_length, finger_nums=3)
-            palm_part.addChild(Index_finger)
-            self.componentList += Index_finger.components
-            for k, v in Index_finger.componentDict.items():
+            palm_part.addChild(index_finger_part)
+            self.componentList.extend(index_finger_part.components)
+            for k, v in index_finger_part.componentDict.items():
                 self.componentDict[f'index_{k}'] = v
 
-            middle_finger = MyModelFinger(self, Point((finger_radius / 3 - palm_length / 6, 0, palm_length)), shaderProg,
+            middle_finger_part = MyModelFinger(self, Point((
+                finger_radius / 3 - palm_length / 6, 0, palm_length)), shaderProg,
                                           finger_radius=finger_radius, finger_height=finger_length, finger_nums=3)
-            palm_part.addChild(middle_finger)
-            self.componentList += middle_finger.components
-            for k, v in middle_finger.componentDict.items():
+            palm_part.addChild(middle_finger_part)
+            self.componentList.extend(middle_finger_part.components)
+            for k, v in middle_finger_part.componentDict.items():
                 self.componentDict[f'middle_{k}'] = v
 
-            ring_finger = MyModelFinger(self, Point((palm_length / 6 - finger_radius / 3, 0, palm_length)), shaderProg,
+            ring_finger_part = MyModelFinger(self, Point((
+                palm_length / 6 - finger_radius / 3, 0, palm_length)), shaderProg,
                                         finger_radius=finger_radius, finger_height=0.9 * finger_length, finger_nums=3)
-            palm_part.addChild(ring_finger)
-            self.componentList += ring_finger.components
-            for k, v in ring_finger.componentDict.items():
+            palm_part.addChild(ring_finger_part)
+            self.componentList.extend(ring_finger_part.components)
+            for k, v in ring_finger_part.componentDict.items():
                 self.componentDict[f'ring_{k}'] = v
 
-            little_finger = MyModelFinger(self, Point((palm_length / 2 - finger_radius, 0, palm_length)), shaderProg,
+            little_finger_part = MyModelFinger(self, Point((
+                palm_length / 2 - finger_radius, 0, palm_length)), shaderProg,
                                           finger_radius=finger_radius, finger_height=0.8 * finger_length, finger_nums=3)
-            palm_part.addChild(little_finger)
-            self.componentList += little_finger.components
-            for k, v in little_finger.componentDict.items():
+            palm_part.addChild(little_finger_part)
+            self.componentList.extend(little_finger_part.components)
+            for k, v in little_finger_part.componentDict.items():
                 self.componentDict[f'little_{k}'] = v
 
             # thumb finger rotation limit
-            thumb_finger.setPartRotateExtent(0, 0, -90, 0)
-            thumb_finger.setPartRotateExtent(0, vAxis, -30, 30)
-            thumb_finger.setPartRotateExtent(0, wAxis, 0, 0)
+            thumb_finger_part.setPartRotateExtent(0, 0, -90, 0)
+            thumb_finger_part.setPartRotateExtent(0, vAxis, -30, 30)
+            thumb_finger_part.setPartRotateExtent(0, wAxis, 0, 0)
 
-            thumb_finger.setPartRotateExtent(1, 0, 0, 0)
-            thumb_finger.setPartRotateExtent(1, vAxis, 0, 90)
-            thumb_finger.setPartRotateExtent(1, wAxis, 0, 0)
+            thumb_finger_part.setPartRotateExtent(1, 0, 0, 0)
+            thumb_finger_part.setPartRotateExtent(1, vAxis, 0, 90)
+            thumb_finger_part.setPartRotateExtent(1, wAxis, 0, 0)
 
         # left handed
         else:
-            wrist_part.setRotateExtent(wrist_part.uAxis, -45, 15)
             wrist_part.setRotateExtent(wrist_part.vAxis, -15, 5)
             wrist_part.setRotateExtent(wrist_part.wAxis, -45, 90)
 
-            palm_part.setRotateExtent(wrist_part.uAxis, -45, 15)
             palm_part.setRotateExtent(wrist_part.vAxis, -15, 5)
-            palm_part.setRotateExtent(wrist_part.wAxis, 0, 0)
 
-            thumb_finger = MyModelFinger(self, Point((palm_length / 2, 0, palm_length / 4)), shaderProg,
-                                         finger_radius=finger_radius, finger_height=finger_length, finger_nums=2)
-            thumb_finger.setDefaultAngle(45, thumb_finger.vAxis)
-            palm_part.addChild(thumb_finger)
-            self.componentList += thumb_finger.components
-            for k, v in thumb_finger.componentDict.items():
+            thumb_finger_part = MyModelFinger(self, Point((
+                palm_length / 2, 0, palm_length / 4)), shaderProg,
+                                              finger_radius=finger_radius, finger_height=finger_length, finger_nums=2)
+            thumb_finger_part.setDefaultAngle(45, thumb_finger_part.vAxis)
+            palm_part.addChild(thumb_finger_part)
+            self.componentList.extend(thumb_finger_part.components)
+            for k, v in thumb_finger_part.componentDict.items():
                 self.componentDict[f'thumb_{k}'] = v
 
-            Index_finger = MyModelFinger(self, Point((palm_length / 2 - finger_radius, 0, palm_length)), shaderProg,
-                                         finger_radius=finger_radius, finger_height=0.9 * finger_length, finger_nums=3)
-            palm_part.addChild(Index_finger)
-            self.componentList += Index_finger.components
-            for k, v in Index_finger.componentDict.items():
+            index_finger_part = MyModelFinger(self, Point((
+                palm_length / 2 - finger_radius, 0, palm_length)), shaderProg,
+                                              finger_radius=finger_radius, finger_height=0.9 * finger_length, finger_nums=3)
+            palm_part.addChild(index_finger_part)
+            self.componentList.extend(index_finger_part.components)
+            for k, v in index_finger_part.componentDict.items():
                 self.componentDict[f'index_{k}'] = v
 
-            middle_finger = MyModelFinger(self, Point((palm_length / 6 - finger_radius / 3, 0, palm_length)), shaderProg,
+            middle_finger_part = MyModelFinger(self, Point((
+                palm_length / 6 - finger_radius / 3, 0, palm_length)), shaderProg,
                                           finger_radius=finger_radius, finger_height=finger_length, finger_nums=3)
-            palm_part.addChild(middle_finger)
-            self.componentList += middle_finger.components
-            for k, v in middle_finger.componentDict.items():
+            palm_part.addChild(middle_finger_part)
+            self.componentList.extend(middle_finger_part.components)
+            for k, v in middle_finger_part.componentDict.items():
                 self.componentDict[f'middle_{k}'] = v
 
-            ring_finger = MyModelFinger(self, Point((finger_radius / 3 - palm_length / 6, 0, palm_length)), shaderProg,
+            ring_finger_part = MyModelFinger(self, Point((
+                finger_radius / 3 - palm_length / 6, 0, palm_length)), shaderProg,
                                         finger_radius=finger_radius, finger_height=0.9 * finger_length, finger_nums=3)
-            palm_part.addChild(ring_finger)
-            self.componentList += ring_finger.components
-            for k, v in ring_finger.componentDict.items():
+            palm_part.addChild(ring_finger_part)
+            self.componentList.extend(ring_finger_part.components)
+            for k, v in ring_finger_part.componentDict.items():
                 self.componentDict[f'ring_{k}'] = v
 
-            little_finger = MyModelFinger(self, Point((-palm_length / 2 + finger_radius, 0, palm_length)), shaderProg,
+            little_finger_part = MyModelFinger(self, Point((
+                -palm_length / 2 + finger_radius, 0, palm_length)), shaderProg,
                                           finger_radius=finger_radius, finger_height=0.8 * finger_length, finger_nums=3)
-            palm_part.addChild(little_finger)
-            self.componentList += little_finger.components
-            for k, v in little_finger.componentDict.items():
+            palm_part.addChild(little_finger_part)
+            self.componentList.extend(little_finger_part.components)
+            for k, v in little_finger_part.componentDict.items():
                 self.componentDict[f'little_{k}'] = v
 
             # thumb finger rotation limit
-            thumb_finger.setPartRotateExtent(0, uAxis, -90, 0)
-            thumb_finger.setPartRotateExtent(0, vAxis, -30, 30)
-            thumb_finger.setPartRotateExtent(0, wAxis, 0, 0)
+            thumb_finger_part.setPartRotateExtent(0, uAxis, -90, 0)
+            thumb_finger_part.setPartRotateExtent(0, vAxis, -30, 30)
+            thumb_finger_part.setPartRotateExtent(0, wAxis, 0, 0)
 
-            thumb_finger.setPartRotateExtent(1, uAxis, 0, 0)
-            thumb_finger.setPartRotateExtent(1, vAxis, -90, 0)
-            thumb_finger.setPartRotateExtent(1, wAxis, 0, 0)
+            thumb_finger_part.setPartRotateExtent(1, uAxis, 0, 0)
+            thumb_finger_part.setPartRotateExtent(1, vAxis, -90, 0)
+            thumb_finger_part.setPartRotateExtent(1, wAxis, 0, 0)
 
-        self.thumb_finger = thumb_finger
-        self.Index_finger = Index_finger
-        self.middle_finger = middle_finger
-        self.ring_finger = ring_finger
-        self.little_finger = little_finger
+        self.thumb_finger = thumb_finger_part
+        self.Index_finger = index_finger_part
+        self.middle_finger = middle_finger_part
+        self.ring_finger = ring_finger_part
+        self.little_finger = little_finger_part
 
         # index finger rotation limit
-        Index_finger.setPartRotateExtent(0, uAxis, -90, 0)
-        Index_finger.setPartRotateExtent(0, vAxis, -5, 5)
-        Index_finger.setPartRotateExtent(0, wAxis, 0, 0)
+        index_finger_part.setPartRotateExtent(0, uAxis, -90, 0)
+        index_finger_part.setPartRotateExtent(0, vAxis, -5, 5)
+        index_finger_part.setPartRotateExtent(0, wAxis, 0, 0)
 
-        Index_finger.setPartRotateExtent(1, uAxis, -90, 0)
-        Index_finger.setPartRotateExtent(1, vAxis, 0, 0)
-        Index_finger.setPartRotateExtent(1, wAxis, 0, 0)
+        index_finger_part.setPartRotateExtent(1, uAxis, -90, 0)
+        index_finger_part.setPartRotateExtent(1, vAxis, 0, 0)
+        index_finger_part.setPartRotateExtent(1, wAxis, 0, 0)
 
-        Index_finger.setPartRotateExtent(2, uAxis, -90, 0)
-        Index_finger.setPartRotateExtent(2, vAxis, 0, 0)
-        Index_finger.setPartRotateExtent(2, wAxis, 0, 0)
+        index_finger_part.setPartRotateExtent(2, uAxis, -90, 0)
+        index_finger_part.setPartRotateExtent(2, vAxis, 0, 0)
+        index_finger_part.setPartRotateExtent(2, wAxis, 0, 0)
 
         # middle finger rotation limit
-        middle_finger.setPartRotateExtent(0, uAxis, -90, 0)
-        middle_finger.setPartRotateExtent(0, vAxis, -5, 5)
-        middle_finger.setPartRotateExtent(0, wAxis, 0, 0)
+        middle_finger_part.setPartRotateExtent(0, uAxis, -90, 0)
+        middle_finger_part.setPartRotateExtent(0, vAxis, -5, 5)
+        middle_finger_part.setPartRotateExtent(0, wAxis, 0, 0)
 
-        middle_finger.setPartRotateExtent(1, uAxis, -90, 0)
-        middle_finger.setPartRotateExtent(1, vAxis, 0, 0)
-        middle_finger.setPartRotateExtent(1, wAxis, 0, 0)
+        middle_finger_part.setPartRotateExtent(1, uAxis, -90, 0)
+        middle_finger_part.setPartRotateExtent(1, vAxis, 0, 0)
+        middle_finger_part.setPartRotateExtent(1, wAxis, 0, 0)
 
-        middle_finger.setPartRotateExtent(2, uAxis, -90, 0)
-        middle_finger.setPartRotateExtent(2, vAxis, 0, 0)
-        middle_finger.setPartRotateExtent(2, wAxis, 0, 0)
+        middle_finger_part.setPartRotateExtent(2, uAxis, -90, 0)
+        middle_finger_part.setPartRotateExtent(2, vAxis, 0, 0)
+        middle_finger_part.setPartRotateExtent(2, wAxis, 0, 0)
 
         # ring finger rotation limit
-        ring_finger.setPartRotateExtent(0, uAxis, -90, 0)
-        ring_finger.setPartRotateExtent(0, vAxis, -5, 5)
-        ring_finger.setPartRotateExtent(0, wAxis, 0, 0)
+        ring_finger_part.setPartRotateExtent(0, uAxis, -90, 0)
+        ring_finger_part.setPartRotateExtent(0, vAxis, -5, 5)
+        ring_finger_part.setPartRotateExtent(0, wAxis, 0, 0)
 
-        ring_finger.setPartRotateExtent(1, uAxis, -90, 0)
-        ring_finger.setPartRotateExtent(1, vAxis, 0, 0)
-        ring_finger.setPartRotateExtent(1, wAxis, 0, 0)
+        ring_finger_part.setPartRotateExtent(1, uAxis, -90, 0)
+        ring_finger_part.setPartRotateExtent(1, vAxis, 0, 0)
+        ring_finger_part.setPartRotateExtent(1, wAxis, 0, 0)
 
-        ring_finger.setPartRotateExtent(2, uAxis, -90, 0)
-        ring_finger.setPartRotateExtent(2, vAxis, 0, 0)
-        ring_finger.setPartRotateExtent(2, wAxis, 0, 0)
+        ring_finger_part.setPartRotateExtent(2, uAxis, -90, 0)
+        ring_finger_part.setPartRotateExtent(2, vAxis, 0, 0)
+        ring_finger_part.setPartRotateExtent(2, wAxis, 0, 0)
 
         # little finger rotation limit
-        little_finger.setPartRotateExtent(0, uAxis, -90, 0)
-        little_finger.setPartRotateExtent(0, vAxis, -5, 5)
-        little_finger.setPartRotateExtent(0, wAxis, 0, 0)
+        little_finger_part.setPartRotateExtent(0, uAxis, -90, 0)
+        little_finger_part.setPartRotateExtent(0, vAxis, -5, 5)
+        little_finger_part.setPartRotateExtent(0, wAxis, 0, 0)
 
-        little_finger.setPartRotateExtent(1, uAxis, -90, 0)
-        little_finger.setPartRotateExtent(1, vAxis, 0, 0)
-        little_finger.setPartRotateExtent(1, wAxis, 0, 0)
+        little_finger_part.setPartRotateExtent(1, uAxis, -90, 0)
+        little_finger_part.setPartRotateExtent(1, vAxis, 0, 0)
+        little_finger_part.setPartRotateExtent(1, wAxis, 0, 0)
 
-        little_finger.setPartRotateExtent(2, uAxis, -90, 0)
-        little_finger.setPartRotateExtent(2, vAxis, 0, 0)
-        little_finger.setPartRotateExtent(2, wAxis, 0, 0)
+        little_finger_part.setPartRotateExtent(2, uAxis, -90, 0)
+        little_finger_part.setPartRotateExtent(2, vAxis, 0, 0)
+        little_finger_part.setPartRotateExtent(2, wAxis, 0, 0)
